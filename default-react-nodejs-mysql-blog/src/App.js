e2ccc4a55f9c0b907e65706f401fd3219e9f876d
DamianStrz <damian.strzalkowski@o2.pl>
2022-11-19 17:12:17 +0100

    Added react router dependency. Basic routes setted.

--- default-react-nodejs-mysql-blog/src/App.js
+++ default-react-nodejs-mysql-blog/src/App.js
@@ -1,9 +1,34 @@
+//Importing components from React Router dependency
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+//Importing components needed for specified Routes
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div>This is Home page</div>,
+	},
+	{
+		path: "/register",
+		element: <Register />,
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+]);
+
 function App() {
-  return (
-    <div>
-      Hello world!
-    </div>
-  );
+	return (
+		<div>
+			<RouterProvider router={router} />
+		</div>
+	);
 }
 
 export default App;
---
+++ default-react-nodejs-mysql-blog/src/pages/Write.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WritePage = () => {
+	return <div>This is posts writing page</div>;
+};
+
+export default WritePage;
