0f8e207457e23ed6076c20a47023e73036262697
DamianStrz <damian.strzalkowski@o2.pl>
2023-01-06 13:28:31 +0100

    Solved problem with connection to database.

--- default-react-nodejs-mysql-blog/api/db.js
+++ default-react-nodejs-mysql-blog/api/db.js
@@ -1,8 +1,15 @@
 import mysql from "mysql2";
+import { config } from "dotenv";
+
+config();
 
 export const db = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
+	host: process.env.NODE_ENV_DB_HOST,
+	user: process.env.NODE_ENV_DB_USERNAME,
+	password: process.env.NODE_ENV_DB_PASSWORD,
+	database: process.env.NODE_ENV_DB_NAME,
 });
+
+
+
+
