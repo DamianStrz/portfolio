d772ab8b7cbc83437a1eca133c5e9e38f0f0e12e
DamianStrz <damian.strzalkowski@o2.pl>
2023-01-21 21:35:11 +0100

    New functionality - uploading files. It doesn't work properly.

--- default-react-nodejs-mysql-blog/api/index.js
+++ default-react-nodejs-mysql-blog/api/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 /*
 Initializing express in app
@@ -11,8 +12,34 @@ Initializing express in app
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
+
+/*
+Using multer diskStorage method to get files from certain folder on client side.
+Also using Date object to add date to files that have the same names.
+*/
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "../public/uploads");
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + file.originalname);
+	},
+});
+
+/*
+Multer creates directory for uploaded files.
+Then in post method we use that directory, and multer "single" method.
+Third argument is a callback.
+*/
+
+const upload = multer({ storage: storage });
+
+app.post("/api/uploads", upload.single("file"), function (req, res) {
+	const file = req.file;
+	res.status(200).json(file.filename);
+});
 
 /*
 At URL host/api/posts/{route_declared_in_posts.js} it shows route declared in posts.js
--- default-react-nodejs-mysql-blog/src/pages/Write.jsx
+++ default-react-nodejs-mysql-blog/src/pages/Write.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
@@ -6,11 +7,35 @@ import "react-quill/dist/quill.snow.css";
 
 const WritePage = () => {
 	const [value, setValue] = useState("");
-	console.log(value);
+	const [title, setTitle] = useState("");
+	const [file, setFile] = useState();
+	const [cat, setCat] = useState("");
+
+	// Function for uploading files = images.
+	const upload = async () => {
+		try {
+			const formData = new FormData();
+			formData.append("file", file);
+			const res = await axios.post("/uploads", formData);
+			console.log(res.data);
+		} catch (err) {
+			alert("You need to upload a file first!");
+		}
+	};
+
+	const handleClick = async (e) => {
+		e.preventDefault();
+		upload();
+	};
+
 	return (
 		<div className="add">
 			<div className="content">
-				<input type="text" placeholder="Enter post title" />
+				<input
+					type="text"
+					placeholder="Enter post title"
+					onChange={(e) => setTitle(e.target.value)}
+				/>
 				<div className="editorContainer">
 					<ReactQuill
 						className="editor"
@@ -34,23 +59,36 @@ const WritePage = () => {
 						type="file"
 						name="file"
 						id="file"
+						onChange={(e) => setFile(e.target.files[0])}
 					/>
 					<label className="file" htmlFor="file">
 						Upload Image
 					</label>
 					<div className="buttons">
 						<button>Save as a draft</button>
-						<button>Upload</button>
+						<button onClick={handleClick}>Publish</button>
 					</div>
 				</div>
 				<div className="item">
 					<h1>Category</h1>
 					<div className="category">
-						<input type="radio" name="category" value="art" id="art" />
+						<input
+							type="radio"
+							name="category"
+							value="art"
+							id="art"
+							onChange={(e) => setCat(e.target.value)}
+						/>
 						<label htmlFor="art">Art</label>
 					</div>
 					<div className="category">
-						<input type="radio" name="category" value="science" id="science" />
+						<input
+							type="radio"
+							name="category"
+							value="science"
+							id="science"
+							onChange={(e) => setCat(e.target.value)}
+						/>
 						<label htmlFor="science">Science</label>
 					</div>
 					<div className="category">
@@ -59,6 +97,7 @@ const WritePage = () => {
 							name="category"
 							value="technology"
 							id="technology"
+							onChange={(e) => setCat(e.target.value)}
 						/>
 						<label htmlFor="technology">Technology</label>
 					</div>
@@ -68,6 +107,7 @@ const WritePage = () => {
 							name="category"
 							value="technology"
 							id="technology"
+							onChange={(e) => setCat(e.target.value)}
 						/>
 						<label htmlFor="technology">Cinema</label>
 					</div>
